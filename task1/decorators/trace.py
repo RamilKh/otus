e06691cf90145aed232e70d4bb829121a333aa79
Ramil Kh <ramilkh.mail@mail.ru>
2020-08-18 20:46:30 +0300

    Task1: Fix function fibonachi

--- task1/decorators/trace.py
+++ task1/decorators/trace.py
@@ -17,22 +17,21 @@ def trace(func):
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        wrapper.count += 1
-        count = wrapper.count
-
         func_name = func.__name__
-        func_args = args
+
+        number = args[0]
+        count = args[1]
+
         space = get_space(count)
 
-        if count == 1:
+        if count == 0:
             print()
-            print(f'{func_name}{func_args} run:')
+            print(f'{func_name}({number}) run:')
 
-        print(f'{space} -> {func_name}{func_args}')
+        print(f'{space} -> {func_name}({number})')
         result = func(*args)
-        print(f'{space} <- {func_name}{func_args} = {result}')
+        print(f'{space} <- {func_name}({number}) = {result}')
 
         return result
 
-    wrapper.count = 0
     return wrapper
--- task1/utils/fib.py
+++ task1/utils/fib.py
@@ -6,39 +6,11 @@ from task1.decorators import trace
 
 
 @trace
-def fib(number):
-    """
-    fib.count: счетчик вложенности.
-    В этой функии управляем счетчиком вложенности отсюда, так как
-    логика подсчета вложенности в декораторе рассчитана на одиночную
-    рекурсию, а здесь за раз вызываем два раза.
-    """
-
-    count = 0
-
-    # если в декораторе count есть, берем оттуда
-    if 'count' in fib.__dict__:
-        count = fib.count
-
-    if number == 0:
-        return 0
-    if number in [1, 2]:
-        return 1
+def fib(number, count=0):
+    if number in [0, 1]:
+        return number
     else:
-        number_1 = number-1
-        number_2 = number-2
-
-        result_1 = fib(number_1)
-
-        # сохранить count, так как result_1 и result_2 на одном уровне
-        fib.count = count
-
-        result_2 = fib(number_2)
-
-        # увеличить count
-        fib.count += count
-
-        return result_1 + result_2
+        return fib(number - 1, count + 1) + fib(number - 2, count + 1)
 
 
 # итератор последовательности чисел
@@ -50,7 +22,7 @@ def do_fib(limit):
 
     i = 0
     while i < limit:
-        number = fib(i)
+        number = fib(i, 0)
         fib.count = 0
 
         result.append(number)
